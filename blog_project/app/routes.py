038c37333af1c997a6841d53eb3f2fb5a3f7ecb2
Shruti Ghumaliya <ghumaliyashruti2@gmail.com>
2026-03-03 17:30:23 +0530

    its store password in hash formate before this its store in string .

--- blog_project/app/routes.py
+++ blog_project/app/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Post
 from . import db
 from flask_login import login_user, logout_user, login_required
 from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 main = Blueprint("main", __name__)
 
@@ -73,7 +74,12 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
 
-        new_user = User(username=username, password=password)
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists"
+        
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
 
         db.session.add(new_user)
         db.session.commit()
@@ -84,7 +90,7 @@ def register():
 
 # Login 
 
-@main.route("/login", methods=["GET","POST"])
+@main.route("/login", methods=["GET", "POST"])
 def login():
 
     if request.method == "POST":
@@ -92,10 +98,23 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        user = User.query.filter_by(username=username, password=password).first()
+        # Find user in database
+        user = User.query.filter_by(username=username).first()
 
-        if user:
+        # Check username exists AND password is correct
+        if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect("/")
+            return redirect(url_for("main.home"))
+
+        else:
+            return "Invalid username or password"
 
     return render_template("login.html")
+
+# Logout 
+
+@main.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.login"))
